f740042f0c90dec6bb06f19e35c2d2a14c485076
FrancescoL96 <45236222+FrancescoL96@users.noreply.github.com>
2024-06-03 16:13:28 +0200

    feat: the provider/resources/metrics are now pulled from a dedicated set of CRs (#20)
    This change allows further configuration of the exporter. It adds the ability to decide the metrics for each resource and each provider.

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,6 +1,41 @@
 package config
 
+import operatorPackage "github.com/krateoplatformops/finops-operator-exporter/api/v1"
+
 type Kind struct {
 	Kind   string `json:"kind"`
 	Status string `json:"status"`
+}
+
+type ProviderConfig struct {
+	Spec ProviderConfigSpec `json:"spec" yaml:"spec"`
+}
+
+type ResourceConfig struct {
+	Spec ResourceConfigSpec `json:"spec" yaml:"spec"`
+}
+
+type MetricConfig struct {
+	Spec MetricConfigSpec `json:"spec" yaml:"spec"`
+}
+
+type ProviderConfigSpec struct {
+	ResourcesRef []operatorPackage.ObjectRef `json:"resourcesRef" yaml:"resourcesRef"`
+}
+
+type ResourceConfigSpec struct {
+	ResourceFocusName string                      `json:"resourceFocusName" yaml:"resourceFocusName"`
+	MetricsRef        []operatorPackage.ObjectRef `json:"metricsRef" yaml:"metricsRef"`
+}
+
+type MetricConfigSpec struct {
+	MetricName string   `json:"metricName" yaml:"metricName"`
+	Endpoint   Endpoint `json:"endpoint" yaml:"endpoint"`
+	Interval   string   `json:"interval" yaml:"interval"`
+	Timespan   string   `json:"timespan" yaml:"timespan"`
+}
+
+type Endpoint struct {
+	ResourcePrefix string `json:"resourcePrefix" yaml:"resourcePrefix"`
+	ResourceSuffix string `json:"resourceSuffix" yaml:"resourceSuffix"`
 }
--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -6,7 +6,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"net/url"
+	urlPackage "net/url"
 	"reflect"
 	"regexp"
 	"strconv"
@@ -24,104 +24,116 @@ import (
 	"k8s.io/client-go/rest"
 )
 
+type ResourceIdTypeCombo struct {
+	ResourceId   string
+	ResourceType string
+}
+
 var (
-	ResourceIds []string
-	metricName  = url.QueryEscape("Percentage CPU")
-	timespan    = "month"
-	interval    = "PT15M"
+	ResourceList            []configPackage.ResourceConfigSpec
+	ResourceIdTypeComboList []ResourceIdTypeCombo
 )
 
 func StartNewExporters(config operatorPackage.ExporterScraperConfig) error {
-	inClusterConfig, err := rest.InClusterConfig()
-	if err != nil {
-		return err
-	}
-
-	clientset, err := kubernetes.NewForConfig(inClusterConfig)
+	clientset, err := GetClientSet()
 	if err != nil {
 		return err
 	}
 
 	// For each resourceId found in the FOCUS report
-	for i, resourceId := range ResourceIds {
-		// The name for the ExporterScraperConfig to be created now
-		name := "exporterscraperconfig-" + strings.TrimSuffix(config.Spec.ExporterConfig.Name, "-exporter") + "-res" + strconv.FormatInt(int64(i), 10)
-		// Compute the URL starting from this exporter's URL
-		urlDomainRegex := regexp.MustCompile(`^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/?\n]+)(:[0-9]{4,4}){0,1}`)
-		urlsParts := urlDomainRegex.FindAllString(config.Spec.ExporterConfig.UrlParsed, -1)
-		url := strings.Join(urlsParts, "") + resourceId
+	for i, resourceIdTypeCombo := range ResourceIdTypeComboList {
+		resourceId := resourceIdTypeCombo.ResourceId
+		for _, resource := range ResourceList {
+			if resource.ResourceFocusName == resourceIdTypeCombo.ResourceType {
+				metricsList, err := getMetricsList(clientset, resource)
+				if err != nil {
+					return err
+				}
 
-		// If the URL contains "azure", auto complete with additional (temporary) information
-		switch {
-		case strings.Contains(config.Spec.ExporterConfig.Name, "azure"):
-			url += fmt.Sprintf("/providers/microsoft.insights/metrics?api-version=2023-10-01&metricnames=%s&timespan=%s&interval=%s", metricName, computeTimespan(timespan), interval)
-		}
+				for _, metric := range metricsList {
+					// The name for the ExporterScraperConfig to be created now
+					name := "exporterscraperconfig-" + strings.TrimSuffix(config.Spec.ExporterConfig.Provider.Name, "-exporter") + "-res" + strconv.FormatInt(int64(i), 10)
+					// Compute the URL starting from this exporter's URL
+					urlDomainRegex := regexp.MustCompile(`^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/?\n]+)(:[0-9]{4,4}){0,1}`)
+					urlsParts := urlDomainRegex.FindAllString(config.Spec.ExporterConfig.UrlParsed, -1)
+					url := strings.Join(urlsParts, "") + resourceId
 
-		// Check if the ExporterScraperConfig already exists
-		jsonData, _ := clientset.RESTClient().Get().
-			AbsPath("/apis/finops.krateo.io/v1").
-			Namespace(config.ObjectMeta.Namespace).
-			Resource("exporterscraperconfigs").
-			Name(name).
-			DoRaw(context.TODO())
+					url += fmt.Sprintf(metric.Endpoint.ResourceSuffix, urlPackage.QueryEscape(metric.MetricName), computeTimespan(metric.Timespan), metric.Interval)
 
-		// Check if the CR already exists
-		var crdResponse configPackage.Kind
-		_ = json.Unmarshal(jsonData, &crdResponse)
-		// If it does not exist, build it
-		if crdResponse.Kind == "Status" && crdResponse.Status == "Failure" {
-			exporterScraperConfig := operatorPackage.ExporterScraperConfig{
-				TypeMeta: metav1.TypeMeta{
-					Kind:       "ExporterScraperConfig",
-					APIVersion: "finops.krateo.io/v1",
-				},
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      name,
-					Namespace: config.ObjectMeta.Namespace,
-					OwnerReferences: []metav1.OwnerReference{
-						{
-							APIVersion: "finops.krateo.io/v1",
-							Kind:       "ExporterScraperConfig",
-							Name:       config.ObjectMeta.Name,
-							UID:        config.ObjectMeta.UID,
-						},
-					},
-				},
-				Spec: operatorPackage.ExporterScraperConfigSpec{
-					ExporterConfig: operatorPackage.ExporterConfig{
-						Name:                  name,
-						Url:                   "@RES:" + url,
-						RequireAuthentication: config.Spec.ExporterConfig.RequireAuthentication,
-						AuthenticationMethod:  config.Spec.ExporterConfig.AuthenticationMethod,
-						PollingIntervalHours:  config.Spec.ExporterConfig.PollingIntervalHours,
-						AdditionalVariables:   config.Spec.ExporterConfig.AdditionalVariables,
-					},
-					ScraperConfig: operatorPackage.ScraperConfig{
-						TableName:            config.Spec.ScraperConfig.TableName + "_res",
-						PollingIntervalHours: config.Spec.ScraperConfig.PollingIntervalHours,
-						ScraperDatabaseConfigRef: operatorPackage.ScraperDatabaseConfigRef{
-							Name:      config.Spec.ScraperConfig.ScraperDatabaseConfigRef.Name,
-							Namespace: config.Spec.ScraperConfig.ScraperDatabaseConfigRef.Namespace,
-						},
-					},
-				},
-			}
+					// Check if the ExporterScraperConfig already exists
+					jsonData, _ := clientset.RESTClient().Get().
+						AbsPath("/apis/finops.krateo.io/v1").
+						Namespace(config.ObjectMeta.Namespace).
+						Resource("exporterscraperconfigs").
+						Name(name).
+						DoRaw(context.TODO())
 
-			jsonData, err = json.Marshal(exporterScraperConfig)
-			if err != nil {
-				return err
-			}
-			// Create the object in the cluster
-			_, err := clientset.RESTClient().Post().
-				AbsPath("/apis/finops.krateo.io/v1").
-				Namespace(config.ObjectMeta.Namespace).
-				Resource("exporterscraperconfigs").
-				Name(name).
-				Body(jsonData).
-				DoRaw(context.TODO())
+					// Check if the CR already exists
+					var crdResponse configPackage.Kind
+					_ = json.Unmarshal(jsonData, &crdResponse)
+					// If it does not exist, build it
+					if crdResponse.Kind == "Status" && crdResponse.Status == "Failure" {
+						exporterScraperConfig := operatorPackage.ExporterScraperConfig{
+							TypeMeta: metav1.TypeMeta{
+								Kind:       "ExporterScraperConfig",
+								APIVersion: "finops.krateo.io/v1",
+							},
+							ObjectMeta: metav1.ObjectMeta{
+								Name:      name,
+								Namespace: config.ObjectMeta.Namespace,
+								OwnerReferences: []metav1.OwnerReference{
+									{
+										APIVersion: "finops.krateo.io/v1",
+										Kind:       "ExporterScraperConfig",
+										Name:       config.ObjectMeta.Name,
+										UID:        config.ObjectMeta.UID,
+									},
+								},
+							},
+							Spec: operatorPackage.ExporterScraperConfigSpec{
+								ExporterConfig: operatorPackage.ExporterConfig{
+									Provider: operatorPackage.ObjectRef{
+										Name:      config.Spec.ExporterConfig.Provider.Name,
+										Namespace: config.Spec.ExporterConfig.Provider.Namespace,
+									},
+									Url:                   "@RES:" + url,
+									RequireAuthentication: config.Spec.ExporterConfig.RequireAuthentication,
+									AuthenticationMethod:  config.Spec.ExporterConfig.AuthenticationMethod,
+									PollingIntervalHours:  config.Spec.ExporterConfig.PollingIntervalHours,
+									AdditionalVariables:   config.Spec.ExporterConfig.AdditionalVariables,
+								},
+								ScraperConfig: operatorPackage.ScraperConfig{
+									TableName:            config.Spec.ScraperConfig.TableName + "_res",
+									PollingIntervalHours: config.Spec.ScraperConfig.PollingIntervalHours,
+									ScraperDatabaseConfigRef: operatorPackage.ObjectRef{
+										Name:      config.Spec.ScraperConfig.ScraperDatabaseConfigRef.Name,
+										Namespace: config.Spec.ScraperConfig.ScraperDatabaseConfigRef.Namespace,
+									},
+								},
+							},
+						}
 
-			if err != nil {
-				return err
+						jsonData, err = json.Marshal(exporterScraperConfig)
+						if err != nil {
+							return err
+						}
+						// Create the object in the cluster
+						_, err := clientset.RESTClient().Post().
+							AbsPath("/apis/finops.krateo.io/v1").
+							Namespace(config.ObjectMeta.Namespace).
+							Resource("exporterscraperconfigs").
+							Name(name).
+							Body(jsonData).
+							DoRaw(context.TODO())
+
+						if err != nil {
+							return err
+						}
+
+					} else {
+						continue
+					}
+				}
 			}
 		}
 	}
@@ -129,19 +141,16 @@ func StartNewExporters(config operatorPackage.ExporterScraperConfig) error {
 }
 
 func computeTimespan(timespanName string) string {
-	dateFormat := "yyyy-mm-dd"
+	// Format: yyyy-mm-dd
+	dateFormat := "2006-01-02"
 	switch timespanName {
 	case "day":
-		interval = "PT5M"
 		return time.Now().Format(dateFormat) + "/" + time.Now().AddDate(0, 0, -1).Format(dateFormat)
 	case "month":
-		interval = "PT15M"
 		return time.Now().Format(dateFormat) + "/" + time.Now().AddDate(0, -1, 0).Format(dateFormat)
 	case "year":
-		interval = "PT6H"
 		return time.Now().Format(dateFormat) + "/" + time.Now().AddDate(-1, 0, 0).Format(dateFormat)
 	}
-	interval = "PT15M"
 	return time.Now().Format(dateFormat) + "/" + time.Now().AddDate(0, -1, 0).Format(dateFormat)
 }
 
@@ -157,8 +166,8 @@ func TryParseResponseAsFocusJSON(jsonData []byte) ([]byte, error) {
 	var focusConfigList operatorPackageFocus.FocusConfigList
 	err := json.Unmarshal(jsonData, &focusConfigList)
 	if err != nil {
-		fmt.Println("Parsing failed:", err)
-		fmt.Println(string(jsonData))
+		fmt.Println("\t", "Parsing failed:", err)
+		//fmt.Println(string(jsonData))
 		return []byte{}, err
 	}
 
@@ -190,7 +199,6 @@ func GetOutputStr(configList operatorPackageFocus.FocusConfigList) string {
 }
 
 func GetStringValue(value any) string {
-
 	str, ok := value.(string)
 	if ok {
 		return str
@@ -241,4 +249,102 @@ func GetIndexOf(records [][]string, toFind string) (int, error) {
 		}
 	}
 	return -1, errors.New(toFind + " not found")
+}
+
+func InitializeResourcesWithProvider(config operatorPackage.ExporterScraperConfig) ([]string, error) {
+	clientset, err := GetClientSet()
+	if err != nil {
+		return []string{}, err
+	}
+
+	jsonData, err := clientset.RESTClient().
+		Get().
+		AbsPath("/apis/finops.krateo.io/v1").
+		Namespace(config.Spec.ExporterConfig.Provider.Namespace).
+		Resource("providerconfigs").
+		Name(config.Spec.ExporterConfig.Provider.Name).
+		DoRaw(context.TODO())
+	if err != nil {
+		return []string{}, err
+	}
+
+	var providerConfig configPackage.ProviderConfig
+	err = json.Unmarshal(jsonData, &providerConfig)
+	if err != nil {
+		return []string{}, err
+	}
+
+	providerConfigSpec := providerConfig.Spec
+
+	fmt.Println("Found provider", config.Spec.ExporterConfig.Provider.Name)
+	resourceStrings := []string{}
+
+	for _, resource := range providerConfigSpec.ResourcesRef {
+		jsonData, err := clientset.RESTClient().
+			Get().
+			AbsPath("/apis/finops.krateo.io/v1").
+			Namespace(resource.Namespace).
+			Resource("resourceconfigs").
+			Name(resource.Name).
+			DoRaw(context.TODO())
+		if err != nil {
+			return []string{}, err
+		}
+		var resourceConfig configPackage.ResourceConfig
+		err = json.Unmarshal(jsonData, &resourceConfig)
+		if err != nil {
+			return []string{}, err
+		}
+		resourceConfigSpec := resourceConfig.Spec
+		resourceStrings = append(resourceStrings, resourceConfigSpec.ResourceFocusName)
+		ResourceList = append(ResourceList, resourceConfigSpec)
+
+		fmt.Println("Found resource", resourceConfigSpec.ResourceFocusName)
+	}
+
+	return resourceStrings, nil
+}
+
+func GetClientSet() (*kubernetes.Clientset, error) {
+	inClusterConfig, err := rest.InClusterConfig()
+	if err != nil {
+		return &kubernetes.Clientset{}, err
+	}
+
+	inClusterConfig.APIPath = "/apis"
+	inClusterConfig.GroupVersion = &operatorPackage.GroupVersion
+
+	clientset, err := kubernetes.NewForConfig(inClusterConfig)
+	if err != nil {
+		return &kubernetes.Clientset{}, err
+	}
+	return clientset, nil
+}
+
+func getMetricsList(clientset *kubernetes.Clientset, resource configPackage.ResourceConfigSpec) ([]configPackage.MetricConfigSpec, error) {
+	result := []configPackage.MetricConfigSpec{}
+	for _, metric := range resource.MetricsRef {
+		jsonData, err := clientset.RESTClient().
+			Get().
+			AbsPath("/apis/finops.krateo.io/v1").
+			Namespace(metric.Namespace).
+			Resource("metricconfigs").
+			Name(metric.Name).
+			DoRaw(context.TODO())
+		if err != nil {
+			return result, err
+		}
+		var metricConfig configPackage.MetricConfig
+		err = json.Unmarshal(jsonData, &metricConfig)
+		if err != nil {
+			return result, err
+		}
+
+		metricConfigSpec := metricConfig.Spec
+
+		result = append(result, metricConfigSpec)
+		fmt.Println("\t", "Found metric", metricConfigSpec.MetricName, metricConfigSpec.Interval, metricConfigSpec.Timespan)
+	}
+
+	return result, nil
 }
